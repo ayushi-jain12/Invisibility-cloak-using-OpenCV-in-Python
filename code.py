2385c981d5284a0ebc74d3d3c8655a98abd7d888
ayushi-jain12 <93311446+ayushi-jain12@users.noreply.github.com>
2021-11-25 18:48:02 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,77 +1,52 @@
+#importing required libraries
 import cv2
+import numpy
 import time
-import numpy as np
 
-## Preparation for writing the ouput video
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
-##reading from the webcam
-cap = cv2.VideoCapture(0)
-
-## Allow the system to sleep for 3 seconds before the webcam starts
+#capturing background from webcam
+capture = cv2.VideoCapture(0)  
+#giving time to webcam to warm up
 time.sleep(3)
-count = 0
-background = 0
+background=0
+for i in range(30):
+	ret,background = capture.read()
 
-## Capture the background in range of 60
-for i in range(60):
-    ret, background = cap.read()
-background = np.flip(background, axis=1)
+#flipping the background
+background = numpy.flip(background,axis=1)
 
-## Read every frame from the webcam, until the camera is open
-while (cap.isOpened()):
-    ret, img = cap.read()
-    if not ret:
-        break
-    count += 1
-    img = np.flip(img, axis=1)
+while(capture.isOpened()):
+    #capturing the live frame
+	ret, img = capture.read()
+	
+	# Flipping the image 
+	img = numpy.flip(img,axis=1)
+	
+	# Converting image to HSV color space.
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	
+	# Range for lower red
+	red_lower = numpy.array([0,120,70])
+	red_upper = numpy.array([10,255,255])
+	mask1 = cv2.inRange(hsv,red_lower,red_upper)
+	
+	# Range for upper red
+	red_lower = numpy.array([170,120,70])
+	red_upper = numpy.array([180,255,255])
+	mask2 = cv2.inRange(hsv,red_lower,red_upper)
+	
+	# Adding the two masks in order to generate the final mask.
+	mask = mask1+mask2
+	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, numpy.ones((5,5),numpy.uint8))
+	
+	# Replacing pixels corresponding to cloak with the background pixels.
+	img[numpy.where(mask==255)] = background[numpy.where(mask==255)]
+	cv2.imshow('Invisibility cloak',img)  
+	k = cv2.waitKey(10)
+	if k == 27: # Esc key to stop
+		break
 
-    ## Convert the color space from BGR to HSV
-    hsv = cv2.cv2tColor(img, cv2.COLOR_BGR2HSV)
-
-    ## Generat masks to detect red color
-    
-    ##YOU CAN CHANGE THE COLOR VALUE BELOW ACCORDING TO YOUR CLOTH COLOR
-    lower_red = np.array(0, 120, 50)
-    upper_red = np.array([10, 255,255])
-    mask1 = cv2.inRange(hsv, lower_red, upper_red)
-
-    lower_red = np.array([170, 120, 70])
-    upper_red = np.array([180, 255, 255])
-    mask2 = cv2.inRange(hsv, lower_red, upper_red)
-
-    mask1 = mask1 + mask2
-
-    ## Open and Dilate the mask image
-    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
-    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
-
-    ## Create an inverted mask to segment out the red color from the frame
-    mask2 = cv2.bitwise_not(mask1)
-
-    ## Segment the red color part out of the frame using bitwise and with the inverted mask
-    res1 = cv2.bitwise_and(img, img, mask=mask2)
-
-    ## Create image showing static background frame pixels only for the masked region
-    res2 = cv2.bitwise_and(background, background, mask=mask1)
-
-    ## Generating the final output and writing
-    finalOutput = cv2.addWeighted(res1, 1, res2, 1, 0)
-    out.write(finalOutput)
-    cv2.imshow("magic", finalOutput)
-    cv2.waitKey(1)
-
-
-cap.release()
-out.release()
+# Release webcam 
+capture.release()
+# De-allocating any associated memory usage  
 cv2.destroyAllWindows()
-
-
-#colors code
-
-#skin color Values
-#lower_red = np.array([0, 0, 70])
-#upper_red = np.array([100, 255,255])
-# mask1 = cv2.inRange(hsv, lower_red, upper_red)
-#-----------------------
